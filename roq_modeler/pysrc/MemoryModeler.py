33ea28dca011cf4c93bc65730367e6a806c0cbce
Koki Nagahama <hervtea.midori@gmail.com>
2021-01-12 16:15:04 +0900

    almost completed

--- roq_modeler/pysrc/MemoryModeler.py
+++ roq_modeler/pysrc/MemoryModeler.py
@@ -5,6 +5,7 @@
 # =============================
 
 ## Regression Kit
+import time
 import numpy as np
 import pandas as pd
 import sklearn
@@ -14,88 +15,146 @@ from concurrent.futures import ThreadPoolExecutor
 ## ROS2
 import rclpy
 from rclpy.node import Node
-from ******.msg import ******	## Subscribe
-from ******.msg import ******	## Publish
+from roq_messrv.msg import MemProcMsg, ModelParametersMsg	## Subscribe, Publish
 
-# Debug Utilities
+## Debug Utilities
+import pprint
 import traceback
 
 class MemoryModeler(Node):
-	# Node & Topic Name
+	## Node & Topic Name
 	NODENAME = 'mem_modeler'
+	PUBTOPIC = 'model_params'
+	SUBTOPIC = 'memproc_data'
 	r_INTERVAL = 90
 
+	## Instances
+	thread_list = []
+	#executor = ThreadPoolExecutor(max_workers = 8)
+
+	## Model Parameters
+	#MID = -1
+	p_buffer = 0.00
+	p_cache = 0.00
+	p_heap_bringup, p_heap_teleop = 0.00, 0.00
+	p_stack_bringup, p_stack_teleop = 0.00, 0.00
+	p_intercept = 0.00
+	
 	## single robot version
-	self.elements_of_X = \
-		['buffer_szMB', \
+	elements_of_X = [
+		'buffer_szMB', \
 		'cache_szMB', \
 		'heap_bringup_szMB', 'heap_teleop_szMB', \
 		'stack_bringup_szMB', 'stack_teleop_szMB']
-	self.element_of_Y = ['system_mem_per']
+	element_of_Y = ['system_mem_per']
+	mem_data_block = np.empty(len(elements_of_X) + len(element_of_Y))
+	is_init = True
 
 	def __init__(self):
 		super().__init__(self.NODENAME)
 		self.get_logger().info('{} initializing...'.format(self.NODENAME))
 
 		## Create ROS2 instance
-		self.pub = self.create_publisher(******, "model_params")
-		self.time_period = 1.00
-		self.timer = self.create_timer(self.time_period, self.modeler_pub_callback)
-
-		self.sub = self.create_subscription(ModelParams, "model_params", self.modeler_sub_callback)
-
-		## Create REG instance
-		self.data_array = np.array()
+		self.pub = self.create_publisher(ModelParametersMsg, self.PUBTOPIC, 10)
+		self.timer = self.create_timer(1.00, self.modeler_pub_callback)
+		self.sub = self.create_subscription(MemProcMsg, self.SUBTOPIC, self.modeler_sub_callback, 10)
 	
 	def __del__(self):
-		self.get_logger().info('{} done.'.format(self.NODENAME))
+		self.get_logger().info("%s done." % self.NODENAME)
 
 	## Multi Linear Regression @Multi-Threading
-	def regression_part(self, dataflame):
+	def regression_part(self, dataframe):
+		#self.get_logger().info('into REG')
 		clf = linear_model.LinearRegression()
-		clf.fit(dataflame[self.elements_of_X], dataflame[self.element_of_Y])
+		clf.fit(dataframe[self.elements_of_X], dataframe[self.element_of_Y])
+
+		## Setting message value
+		predict_params = clf.coef_[0]
+		self.get_logger().info('--> predict_params: {}'.format(
+			np.round(np.append(clf.coef_[0], clf.intercept_), decimals = 4))
+		)
+		self.p_buffer = predict_params[0]
+		self.p_cache = predict_params[1]
+		self.p_heap_bringup, self.p_heap_teleop = predict_params[2], predict_params[3]
+		self.p_stack_bringup, self.p_stack_teleop = predict_params[4], predict_params[5]
+		self.p_intercept = clf.intercept_[0]
 
 	## callback function when publish message
 	def modeler_pub_callback(self):
 		## Message setting
-		msg = MemProc()
-		msg = 
+		msg = ModelParametersMsg()
+		#msg.machine_id = self.MID
+		msg.p_buffer = self.p_buffer
+		msg.p_cache = self.p_cache
+		msg.p_heap_bringup, msg.p_heap_teleop	= self.p_heap_bringup, self.p_heap_teleop
+		msg.p_stack_bringup, msg.p_stack_teleop = self.p_stack_bringup, self.p_stack_teleop
+		msg.p_intercept = self.p_intercept
+
+		## Send message
+		self.pub.publish(msg)
 	
 	## callback function when subscribe message
 	"""
 		Discription:
 			Checking and storing elements arriving by Subscribe, data frame conversion
+			value of valid_flag:
+			- 0: OK
+			- 1: Process killed
+			- 2: NG
 	"""
 	def modeler_sub_callback(self, message):
-		mem_data_arrival = np.array(
-			[
-				message.******, message.******, message.******, message.******, message.******, message.******
-			]
-		)
-		
-		if not(len(mem_data_arrival) == len(self.elements_of_X) + len(selt.element_of_Y)):
-			raise NANTOKAException
+		#start = time.time()
+		executor = ThreadPoolExecutor(max_workers = 2)
+		valid_flag = message.is_valid
+		if valid_flag <= 1:
+			#self.MID = message.machine_id
+			mem_data_arrival = np.array(
+				[
+					message.system,
+					message.buffer_sz,
+					message.cache_sz,
+					message.heap_bringup_sz, message.heap_teleop_sz,
+					message.stack_bringup_sz, message.stack_teleop_sz
+				]
+			)
+			if not(len(mem_data_arrival) == len(self.elements_of_X) + len(self.element_of_Y)):
+				raise ValueError
+			else:
+				if self.is_init == True:
+					self.mem_data_block = np.vstack( (self.mem_data_block, mem_data_arrival) )
+					self.mem_data_block = np.delete(self.mem_data_block, 0, axis = 0)
+					self.is_init = False
+				else:
+					self.mem_data_block = np.vstack( (self.mem_data_block, mem_data_arrival) )
+				
+				self.get_logger().info('mem_data_block: {}, valid_flag: {}'.format(len(self.mem_data_block), valid_flag))
+				if len(self.mem_data_block) >= self.r_INTERVAL or valid_flag == 1:
+	
+					dataframe = pd.DataFrame(self.mem_data_block)
+					dataframe.columns = self.element_of_Y + self.elements_of_X
+
+					## Reinitialize mem_data_block
+					self.mem_data_block = np.empty(len(self.elements_of_X) + len(self.element_of_Y))
+					self.is_init = True
+
+					## Multiple regression model building
+					self.thread_list.append( executor.submit(self.regression_part, dataframe) )
+
 		else:
-			try:
-				mem_data_block = np.vstack( (mem_data_block, mem_data_arrival) )
-			except NameError:
-				mem_data_block = np.empty(len(self.elements_of_X) + len(self.element_of_Y))
-				mem_data_block = np.vstack( (mem_data_block, mem_data_arrival) )
-				mem_data_block = np.delete(a, 0, axis = 0)
-			finally:
-				if len(mem_data_block) >= 90:
-					dataflame = pd.DataFrame(mem_data_block)
-					dataflame.columns = self.element_of_Y + self.elements_of_X
-					predict_params = regression_part(dataflame)
+			raise ValueError
+		#end = time.time()
+		self.get_logger().info('rap time: {:.4f}'.format(end - start))
 
 
-def main():
+def main(args = None):
 	rclpy.init(args = args)
 	node = MemoryModeler()
 	try:
 		rclpy.spin(node)
-	except NANTOKAException:
-		print('Exception raised..')
+	except KeyboardInterrupt:
+		print('\nGot Ctrl+C.  System is stopped..')
+	except Exception:
+		print('\nException raised..  System will be shutdown..')
 		traceback.print_exc()
 	finally:
 		node.destroy_node()
---
+++ roq_modeler/pysrc/tester.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env /usr/bin/python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------
+# ROS2 Node 送信側
+#
+# The MIT License (MIT)
+# Copyright (C) 2019 myasu.
+# -----------------------------------------------
+
+import rclpy
+from rclpy.node import Node
+
+import random
+from roq_messrv.msg import MemProcMsg
+
+class MyPublisher(Node):
+	SELFNODE = "testpub"
+	SELFTOPIC = "memproc_data"
+
+	def __init__(self):
+		super().__init__(self.SELFNODE)
+		self.get_logger().info("%s initializing..." % (self.SELFNODE))
+		self.pub = self.create_publisher(MemProcMsg, self.SELFTOPIC, 10)
+		self.create_timer(1.00, self.callback)
+		self.get_logger().info("%s do..." % self.SELFNODE)
+		self.count = 1
+
+	def __del__(self):
+		self.get_logger().info("%s done." % self.SELFNODE)
+
+	def callback(self):
+		self.get_logger().info("Publish [%s]" % (self.count))
+		msg = MemProcMsg()
+		
+		msg.is_valid = 0
+		msg.system = 60.5000 + random.uniform(0, 4.5000)
+		msg.buffer_sz = 308 + random.randint(-50, 50)
+		msg.cache_sz = 400 + random.randint(-50, 50)
+		msg.heap_bringup_sz = 1000 + random.randint(0, 200)
+		msg.heap_teleop_sz = 200 + random.randint(0, 50)
+		msg.stack_bringup_sz = 8
+		msg.stack_teleop_sz = 40
+
+		self.pub.publish(msg)
+		self.count += 1
+
+def main(args = None):
+	try:
+		rclpy.init(args = args)
+		node = MyPublisher()
+		rclpy.spin(node)
+	except KeyboardInterrupt:
+		pass
+	finally:
+		node.destroy_node()
+		rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
