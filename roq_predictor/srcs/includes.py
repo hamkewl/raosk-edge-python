e37c1b337c1e4036653bf0910f713019fde49128
Koki Nagahama <hamstick.devel@gmail.com>
2021-02-10 20:59:37 +0900

    modified: caused by import error

---
+++ roq_predictor/srcs/includes.py
@@ -0,0 +1,49 @@
+# =============================
+# Includes.py
+# @discription	Class file for Predictor.py
+# @Author				Koki Nagahama (@hamstick)
+# =============================
+
+# from Meminfo
+class MemProc:
+  vgid = -1
+  childs = []
+  buffer_sz = 0.
+  cache_sz = 0.
+  heap_sz = 0.
+  stack_sz = 0.
+
+  def __init__(self):
+    pass
+
+#from Netinfo
+class NwProc:
+  n_send = 0
+  n_receive = 0
+
+  def __init__(self):
+    pass
+
+# from Predictor.Mem
+class MemParams:
+  vgid = -1
+  childs = []
+  p_buffer = 0.
+  p_cache = 0.
+  p_heap = 0.
+  p_stack = 0.
+  p_intercept = 0.
+
+  def __init__(self):
+    pass
+
+# from Predictor.Network
+class NwParams:
+  p_ave_send = 0.
+  p_ave_receive = 0.
+
+  def __init__(self):
+    pass
+
+def main():
+  pass
